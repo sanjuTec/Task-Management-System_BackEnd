d21c4bcb4f102b8f92b83699048dffe29d80ae07
kelum <kelumnagodavithana1997@gmail.com>
2024-07-01 13:18:18 +0530

    initial commit

---
+++ src/main/java/com/tms/task_springboot/TaskSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tms.task_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/task_springboot/TaskSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.task_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
